82c4861cc9397aef40f989b76c58e7fef5f9e7eb
joshinehajoshi <nehajoshigkp@gmail.com>
2023-01-13 13:27:25 +0530

    class pratice

---
+++ src/components/classPrac.js
@@ -0,0 +1,22 @@
+// class Animal() {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age
+//     }
+
+//     nameFun() {
+//         console.log(`name is ${this.name}`)
+//     }
+//     ageFun() {
+//         console.log(`age is ${this.age}`)
+//     }
+// }
+
+// class Monkey extends Animal {
+//     eatBanana() {
+//         console.log(`Monkey eating banana`);
+//     }
+// }
+
+// let mon  = new Monkey("chunku", 20);
+// mon.eatBanana()
